66535e076cbe2e220797d772ee0df0fe81691d17
Lucas Pimenta <52849575+Jinkogule@users.noreply.github.com>
2020-10-09 23:26:23 -0300

    Update ex4_a.cpp

--- ex4_a.cpp
+++ ex4_a.cpp
@@ -1,3 +1,12 @@
+/*4. Escreva um algoritmo que dada uma pilha P, inverta a ordem dos
+elementos de P. Seu algoritmo deve usar espaço auxiliar constante
+(somente variáveis locais e nenhum vetor extra) e:
+a) uma fila
+b) duas pilhas
+c) uma pilha
+Seu algoritmo deve manipular as pilhas e filas por suas interfaces padrão
+(somente métodos do TAD). 5. Escreva um algoritmo que dada*/
+
 #include <iostream>
 #include <stack>
 #include <queue>
